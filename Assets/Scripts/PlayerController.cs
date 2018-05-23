b7c47b8a757cf0af5bdde213ee8f625aee0c5112
tracymeng2000 <tracymeng2000@gmail.com>
2018-05-23 15:54:51 -0700

    Counting, Winning Text
    Added score and winning text

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	public Text countText;
+	public Text winText;
 	private Rigidbody rb;
+	private int count;
 
 	void Start(){
 		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText();
+		winText.text = "";
 	}
+
 	void FixedUpdate(){
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
@@ -18,4 +26,18 @@ public class PlayerController : MonoBehaviour {
 		rb.AddForce (movement * speed);
 	}
 
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			count++;
+			SetCountText();
+		}
+	}
+
+	void SetCountText (){
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) {
+			winText.text = "You win";
+		}
+	}
 }
